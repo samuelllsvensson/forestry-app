8345facd6468d6c5279c40f287c231117092b3d2
samochips <samuel.svensson.95@gmail.com>
2021-01-16 17:53:31 +0100

    Fleshed out AgeTable, added tabs and restructuring

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 // import Moment from 'react-moment';
 
 // import firebase from "firebase/app";
@@ -8,56 +8,131 @@ import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+import { green } from '@material-ui/core/colors';
 
+// import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 //import { metadata} from "./geojsonSource.js"
 // import {geoPointToArrayList, getColor} from './Utils.js'
 
+import SummaryComponent from './components/SummaryComponent';
+import AgeTableComponent from './components/AgeTableComponent';
 import MetadataComponent from './components/MetadataComponent';
-//import AgeTableComponent from './components/AgeTableComponent';
+
 import Map from './components/Map';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      // Purple and green play nicely together.
+      main: green[800],
+    },
+    secondary: {
+      // This is green.A700 as hex.
+      main: '#ff9100',
+    },
+  },
+});
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
   },
   paper: {
-    padding: theme.spacing(2),
+    padding: theme.spacing(1), 
     textAlign: 'center',
     color: theme.palette.text.secondary,
   },
 }));
 
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          {children}
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
 const App = () => {
   const classes = useStyles();
-  //const [map, setMap] = useState(null);
-  //const mapContainer = useRef(null);
-  const [metaData, setMetaData] = useState({});
-
-  // Write to metadata collection 
-  // db.collection("data").doc("0").set(data)
-  // .then(function() {
-  //     console.log("Document successfully written!");
-  // })
-  // .catch(function(error) {
-  //     console.error("Error writing document: ", error);
-  // });
-
+  
   useEffect(() => {
 
   }, []);
 
+  const [value, setValue] = React.useState(0);
+  const [clickedID, setClickedID] = React.useState(1); // the lifted state
 
+  const sendDataToParent = (index) => { // the callback. Use a better name
+    console.log(index);
+    setClickedID(index);
+    setValue(2);
+
+  };
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
   return (
     <div className={classes.root}>
-      <Grid container spacing={1}>
-        <Grid item xl={7}>
-          <Map/> 
-        </Grid>
-        <Grid item xl={5}>
-          <Paper className={classes.paper}><MetadataComponent dataParentToChild = {metaData}/></Paper> 
-          {/* <Paper className={classes.paper}><AgeTableComponent ageData={"test"}/></Paper>  */}
-        </Grid>
-      </Grid>
+      <ThemeProvider theme={theme}>
+        <Grid container spacing={1}>
+          <Grid item sm={5}>
+            <Map sendDataToParent={sendDataToParent}/> 
+          </Grid>
+          <Grid item sm={7}>
+            <AppBar position="static">
+              <Tabs value={value} onChange={handleChange}>
+                <Tab label="Sammanställning" {...a11yProps(0)} />
+                <Tab label="Åldersklassfördelning" {...a11yProps(1)} />
+                <Tab label="Avdelningsinformation" {...a11yProps(2)} />
+              </Tabs>
+            </AppBar>
+            <TabPanel value={value} index={0}>
+            <Paper><SummaryComponent /></Paper>
+            </TabPanel>
+            <TabPanel value={value} index={1}>
+              <AgeTableComponent ageData={"test"}/>
+            </TabPanel>
+            <TabPanel value={value} index={2}>
+              <MetadataComponent dataParentToChild = {clickedID}/>
+            </TabPanel>
+              
+          </Grid>
+        </Grid>    
+      </ThemeProvider>
     </div>
   );
 }
--- src/components/AgeTableComponent.js
+++ src/components/AgeTableComponent.js
@@ -1,48 +1,78 @@
-import React, { useState } from 'react';
-import ReactDOM from "react-dom";
+import React, { useState, useEffect } from 'react';
+import db from '../utils/firestore.js' 
+import {tableText} from "../utils/tooltips.js"
+
 import { makeStyles } from "@material-ui/core/styles";
+
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
-import Input from "@material-ui/core/Input";
+import TextField from "@material-ui/core/TextField";
 import Paper from "@material-ui/core/Paper";
 import IconButton from "@material-ui/core/IconButton";
+import Tooltip from '@material-ui/core/Tooltip';
+import LinearProgress from '@material-ui/core/LinearProgress';
 // Icons
 import EditIcon from "@material-ui/icons/EditOutlined";
 import DoneIcon from "@material-ui/icons/DoneAllTwoTone";
 import RevertIcon from "@material-ui/icons/NotInterestedOutlined";
+import { Chart } from 'primereact/chart';
 
 const useStyles = makeStyles(theme => ({
   root: {
-    width: "100%",
-    marginTop: theme.spacing(3),
-    overflowX: "auto"
+    width: "80%",
+    marginLeft: "auto",
+    marginRight: "auto"
   },
   table: {
+    marginLeft: "auto",
+    marginRight: "auto",
     minWidth: 650
   },
   selectTableCell: {
-    width: 60
+    width: 20,
+    height: 0,
+    padding: 0
+
   },
   tableCell: {
-    width: 130,
-    height: 40
+    width: 30,
+    height: 0,
+    padding: 0
   },
   input: {
-    width: 130,
-    height: 40
+    width: 40,
+    height: 30,
+    flex: 1,
+    "& input::-webkit-clear-button, & input::-webkit-outer-spin-button, & input::-webkit-inner-spin-button": {
+      display: "none"
+}
   }
 }));
 
-const createData = (name, calories, fat, carbs, protein) => ({
-  id: name.replace(" ", "_"),
-  name,
-  calories,
-  fat,
-  carbs,
-  protein,
+const createData = (ageRange, 
+                    area, 
+                    percent, 
+                    hecVolume, 
+                    totVolume,
+                    quality,
+                    growthRate,
+                    pine,
+                    fir,
+                    leaf) => ({
+  id: ageRange.replace(" ", "_"),
+  ageRange,
+  area,
+  percent,
+  hecVolume,
+  totVolume,
+  quality,
+  growthRate,
+  pine,
+  fir,
+  leaf,
   isEditMode: false
 });
 
@@ -50,13 +80,15 @@ const CustomTableCell = ({ row, name, onChange }) => {
   const classes = useStyles();
   const { isEditMode } = row;
   return (
-    <TableCell align="left" className={classes.tableCell}>
+    <TableCell align="center" className={classes.tableCell}>
       {isEditMode ? (
-        <Input
+        <TextField
           value={row[name]}
           name={name}
           onChange={e => onChange(e, row)}
           className={classes.input}
+          size="small"
+          type="number"
         />
       ) : (
         row[name]
@@ -65,15 +97,38 @@ const CustomTableCell = ({ row, name, onChange }) => {
   );
 };
 
-function AgeTableComponent({ageData}) {
-    console.log(ageData)
-  const [rows, setRows] = useState([
-    createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-    createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-    createData("Eclair", 262, 16.0, 24, 6.0)
-  ]);
-  const [previous, setPrevious] = useState({});
+function AgeTableComponent() {
   const classes = useStyles();
+
+  const [rows, setRows] = useState([]);
+  const [previous, setPrevious] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    let temp = []
+    db.collection("ages").orderBy("orderId").get().then(function(querySnapshot) {
+      querySnapshot.forEach(function(doc) {
+          setLoading(false);
+          temp.push(createData(doc.data().ageRange, 
+                     doc.data().area, 
+                     doc.data().percent, 
+                     doc.data().hecVolume, 
+                     doc.data().totVolume, 
+                     doc.data().quality, 
+                     doc.data().growthRate, 
+                     doc.data().pine, 
+                     doc.data().fir, 
+                     doc.data().leaf))
+      });
+      let newArray = sumRows(temp);
+      setRows(state => {
+        return newArray.map(row => {
+          return row;
+        });
+      });
+    });
+  },[]); 
+
 
   const onToggleEditMode = id => {
     setRows(state => {
@@ -86,22 +141,37 @@ function AgeTableComponent({ageData}) {
     });
   };
 
+  const onSaveEditMode = id => {
+    const found = rows.find(element => element.id === id);
+    db.collection("ages").doc(found.ageRange).update(found)
+    .then(function() {
+        console.log("Document successfully written!");
+    })
+    .catch(function(error) {
+        console.error("Error writing document: ", error);
+    });
+    onToggleEditMode(id);
+    console.log("saved to database")
+  };
+
   const onChange = (e, row) => {
+
     if (!previous[row.id]) {
       setPrevious(state => ({ ...state, [row.id]: row }));
     }
-    const value = e.target.value;
+
+    let value = e.target.value;
     const name = e.target.name;
     const { id } = row;
     const newRows = rows.map(row => {
       if (row.id === id) {
-        return { ...row, [name]: value };
+        return { ...row, [name]: parseFloat(value) };
       }
       return row;
     });
     setRows(newRows);
   };
-
+  
   const onRevert = id => {
     const newRows = rows.map(row => {
       if (row.id === id) {
@@ -117,58 +187,165 @@ function AgeTableComponent({ageData}) {
     onToggleEditMode(id);
   };
 
+  const calcMedian = arr => {
+    const mid = Math.floor(arr.length / 2),
+      nums = [...arr].sort((a, b) => a - b);
+    return arr.length % 2 !== 0 ? nums[mid] : (nums[mid - 1] + nums[mid]) / 2;
+  };
+  const sumRows = (rows) => { //
+    let rowsCopy = rows; 
+
+    let area = rows.reduce((r, d) => parseInt(r) + d.area, 0);
+    let percent = rows.reduce((r, d) => r + d.percent, 0);
+
+    var hecVolArray = [];
+    for(var i=0; i<rows.length; i++) {
+      hecVolArray.push(rows[i].hecVolume)
+    }
+    let hecVolumeMedian = calcMedian(hecVolArray);
+    let totVolume = rows.reduce((r, d) => r + d.totVolume, 0);
+
+    var qualityArray = [];
+    for(var j=0; j<rows.length; j++) {
+      qualityArray.push(rows[j].quality)
+    }
+    let qualityMedian = calcMedian(qualityArray);
+
+    var growthRateArray = [];
+    for(var k=0; k<rows.length; k++) {
+      growthRateArray.push(rows[k].growthRate)
+    }
+    let growthRateMedian = calcMedian(growthRateArray);
+
+
+    // hardcoded percents for now
+    let pine = 23
+    let fir = 71
+    let leaf = 6
+    let summedRow = createData("Summa", area, percent, hecVolumeMedian.toFixed(1), totVolume, qualityMedian.toFixed(1), growthRateMedian.toFixed(1), pine, fir, leaf)
+    rowsCopy.push(summedRow) // Append sum row
+    return rowsCopy
+  }
+  
+  let ageLabels = rows.map(a => a.ageRange);
+  ageLabels.pop()
+  let areaData = rows.map(a => a.area);  
+  areaData.pop()
+
+
+  const basicData = {
+    labels: ageLabels,
+    datasets: [
+        {
+            label: 'Åldersklassfördelning idag',
+            backgroundColor: '#42A5F5',
+            data: areaData
+        }
+      ]
+    };
+    let basicOptions = {
+      legend: {
+          labels: {
+              fontColor: '#495057'
+          }
+      },
+      scales: {
+          xAxes: [{
+              ticks: {
+                  fontColor: '#495057'
+              }
+          }],
+          yAxes: [{
+              ticks: {
+                  fontColor: '#495057'
+              }
+          }]
+    }
+  };
+
   return (
+    
     <Paper className={classes.root}>
-      <Table className={classes.table} size="small" aria-label="caption table">
-        {/* <caption>A barbone structure table example with a caption</caption> */}
+    {loading ? (
+
+    <LinearProgress color="secondary" />
+
+  ) : (
+    <div>
+      <Table className={classes.table} size="small" aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell align="left" />
-            <TableCell align="left">Åldersklass</TableCell>
-            <TableCell align="left">Areal</TableCell>
-            <TableCell align="left">Virkesförråd</TableCell>
-            <TableCell align="left">Bonitet</TableCell>
-            <TableCell align="left">Tillväxt</TableCell>
-            <TableCell align="left">Trädslagsfördelning&nbsp;(%)</TableCell>
+            <TableCell align="center" colSpan={1}><Tooltip title={tableText.areal} arrow><span>Åldersklass</span></Tooltip></TableCell> 
+            <TableCell align="center" colSpan={2}><Tooltip title={tableText.areal} arrow><span>Areal</span></Tooltip></TableCell>
+            <TableCell align="center" colSpan={2}><Tooltip title={tableText.storage} arrow><span>Virkesförråd</span></Tooltip></TableCell>
+            <TableCell align="center" colSpan={1}><Tooltip title={tableText.quality} arrow><span>Bonitet</span></Tooltip></TableCell>
+            <TableCell align="center" colSpan={1}><Tooltip title={tableText.growthRate} arrow><span>Tillväxt</span></Tooltip></TableCell>
+            <TableCell align="center" colSpan={3}><Tooltip title={tableText.distribution} arrow><span>Trädslagsfördelning&nbsp;(%)</span></Tooltip></TableCell>
+            <TableCell align="center"></TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell align="center" />
+            <TableCell align="center">ha</TableCell>
+            <TableCell align="center">%</TableCell>
+            <TableCell align="center">m3sk/ha</TableCell>
+            <TableCell align="center">m3sktot</TableCell>
+            <TableCell align="center" colSpan={2}>m3sk/ha och år</TableCell>
+            <TableCell align="center">Tall</TableCell>
+            <TableCell align="center">Gran</TableCell>
+            <TableCell align="center">Löv</TableCell>
+            <TableCell align="center"></TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {rows.map(row => (
             <TableRow key={row.id}>
+              <TableCell component="th" scope="row" align="center">
+                {row.ageRange}
+              </TableCell>
+              <CustomTableCell {...{ row, name: "area", onChange }} />
+              <CustomTableCell {...{ row, name: "percent", onChange }} />
+              <CustomTableCell {...{ row, name: "hecVolume", onChange }} />
+              <CustomTableCell {...{ row, name: "totVolume", onChange }} />
+              <CustomTableCell {...{ row, name: "quality", onChange }} />
+              <CustomTableCell {...{ row, name: "growthRate", onChange }} />
+              <CustomTableCell {...{ row, name: "pine", onChange }} />
+              <CustomTableCell {...{ row, name: "fir", onChange }} />
+              <CustomTableCell {...{ row, name: "leaf", onChange }} />
               <TableCell className={classes.selectTableCell}>
                 {row.isEditMode ? (
                   <>
                     <IconButton
                       aria-label="done"
-                      onClick={() => onToggleEditMode(row.id)}
+                      size="small"
+                      onClick={() => onSaveEditMode(row.id)}
                     >
-                      <DoneIcon />
+                      <DoneIcon fontSize="small"/>
                     </IconButton>
                     <IconButton
                       aria-label="revert"
+                      size="small"
                       onClick={() => onRevert(row.id)}
                     >
-                      <RevertIcon />
+                      <RevertIcon fontSize="small"/>
                     </IconButton>
                   </>
                 ) : (
                   <IconButton
                     aria-label="delete"
+                    size="small"
                     onClick={() => onToggleEditMode(row.id)}
                   >
-                    <EditIcon />
+                    <EditIcon fontSize="small"/>
                   </IconButton>
                 )}
               </TableCell>
-              <CustomTableCell {...{ row, name: "name", onChange }} />
-              <CustomTableCell {...{ row, name: "calories", onChange }} />
-              <CustomTableCell {...{ row, name: "fat", onChange }} />
-              <CustomTableCell {...{ row, name: "carbs", onChange }} />
-              <CustomTableCell {...{ row, name: "protein", onChange }} />
             </TableRow>
           ))}
         </TableBody>
       </Table>
+      <Chart type="bar" data={basicData} options={basicOptions} />
+   </div>
+  )}
     </Paper>
   );
 }
